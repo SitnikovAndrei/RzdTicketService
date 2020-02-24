6e4cdc269c70b4195f739c28a0d11acc0bb0d426
Sitnikov A.A <a.sitnikov@mangotele.com>
2020-02-25 01:26:58 +0300

    add tickets db truncate

--- src/main/java/com/tickets/rzd/controller/RzdController.java
+++ src/main/java/com/tickets/rzd/controller/RzdController.java
@@ -4,16 +4,14 @@ import com.tickets.rzd.entity.TicketEntity;
 import com.tickets.rzd.repository.TicketsRepository;
 import com.tickets.rzd.service.RzdRestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class TestController {
+public class RzdController {
     @Autowired
     private TicketsRepository repository;
 
@@ -22,20 +20,20 @@ public class TestController {
 
     @GetMapping("/tickets/update")
     public ResponseEntity<?> ticketsUpdate() throws Exception{
-        repository.deleteAll();
+        repository.truncate();
         List<TicketEntity> ticketEntityList = rzdRestService.getTicketsByPeriod(30);
         repository.saveAll(ticketEntityList);
         return ResponseEntity.ok("TRUE");
     }
 
-    @GetMapping("/tickets/period/{period}")
-    public List<TicketEntity> getTicketsByPeriod(@PathVariable int period) throws Exception{
-        return rzdRestService.getTicketsByPeriod(period);
-    }
-
-    @GetMapping(value = "/tickets/{date}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getTickets(@PathVariable String date) throws Exception{
-        return ResponseEntity.ok(rzdRestService.getTickets(date));
-    }
+//    @GetMapping("/tickets/period/{period}")
+//    public List<TicketEntity> getTicketsByPeriod(@PathVariable int period) throws Exception{
+//        return rzdRestService.getTicketsByPeriod(period);
+//    }
+//
+//    @GetMapping(value = "/tickets/date/{date}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> getTicketsByDate(@PathVariable String date) throws Exception{
+//        return ResponseEntity.ok(rzdRestService.getTickets(date));
+//    }
 
 }
--- src/main/java/com/tickets/rzd/repository/TicketsRepository.java
+++ src/main/java/com/tickets/rzd/repository/TicketsRepository.java
@@ -1,8 +1,12 @@
 package com.tickets.rzd.repository;
 
 import com.tickets.rzd.entity.TicketEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +23,9 @@ public interface TicketsRepository extends CrudRepository<TicketEntity, Integer>
     );
 
     Optional<TicketEntity> findById(Integer id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "TRUNCATE TABLE public.tickets", nativeQuery = true)
+    void truncate();
 }
